89931645fe33b5183c97a37f5b14c3471ad53c32
Matt Windsor <m.windsor@imperial.ac.uk>
2020-12-14 09:31:59 +0000

    Try fix amplification setup
    Things now use the c4t binaries rather than the (dead) c4f stuff.
    
    I've changed the default Memalloy setup to use partialSC instead of
    RC11+noRMWs.  Hopefully this works out.

--- act_bash/memalloy.sh
+++ act_bash/memalloy.sh
@@ -8,13 +8,13 @@
 
 # TODO(@MattWindsor91): most of these shouldn't be hardcoded.
 
-# The model, relative to Memalloy's models directory, to use to generate
-# violations.
-readonly MEMALLOY_VIOLATES_MODEL="c11_lahav.cat"
+# The model, relative to Memalloy's models directory, that generated programs
+# should witness violations on.
+readonly MEMALLOY_VIOLATES_MODEL="c11_partial.cat" # "c11_lahav.cat"
 
-# The model, relative to Memalloy's models directory, to use to filter
-# read-modify-writes.
-readonly MEMALLOY_NORMWS_MODEL="c11_normws.cat"
+# The model, relative to Memalloy's models directory, that generated programs
+# should witness satisfiability on.
+readonly MEMALLOY_SATISFIES_MODEL="" # "c11_normws.cat"
 
 # The number of events Memalloy should be asked to produce by default.
 readonly DEFAULT_MEMALLOY_EVENTS=4
